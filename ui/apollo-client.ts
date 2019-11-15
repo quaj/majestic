6bd2b7c2628af24c27cf3b40f404114030999b40
Là Đức An Đó <me@clgt.io>
2019-11-15 20:54:48 +0700

    update new

--- ui/apollo-client.ts
+++ ui/apollo-client.ts
@@ -1,42 +1,42 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
-import { WebSocketLink } from "apollo-link-ws";
-import { getMainDefinition } from "apollo-utilities";
-import { split } from "apollo-link";
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import { split } from 'apollo-link'
 
-declare var PRODUCTION: boolean;
+declare var PRODUCTION: boolean
 
-let WS_URL = "ws://localhost:4000";
-let HTTP_URL = "http://localhost:4000";
+let WS_URL = 'ws://localhost:4000'
+let HTTP_URL = 'http://localhost:4000'
 if (PRODUCTION) {
-  WS_URL = `ws://${window.location.host}`;
-  HTTP_URL = `http://${window.location.host}`;
+  WS_URL = `wss://${window.location.host}`
+  HTTP_URL = `https://${window.location.host}`
 }
 
 export function getAPIUrl() {
-  return HTTP_URL;
+  return HTTP_URL
 }
 
 const wsLink = new WebSocketLink({
   uri: WS_URL,
   options: {
-    reconnect: true
-  }
-});
+    reconnect: true,
+  },
+})
 
-const httpLink = new HttpLink({ uri: HTTP_URL });
+const httpLink = new HttpLink({ uri: HTTP_URL })
 
 const link = split(
   ({ query }: any) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === "OperationDefinition" && operation === "subscription";
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
   },
   wsLink,
-  httpLink
-);
+  httpLink,
+)
 
 const client = new ApolloClient({
   link,
-  cache: new InMemoryCache()
-});
+  cache: new InMemoryCache(),
+})
 
-export default client;
+export default client
