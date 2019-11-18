6d281f474bd6dd28f6bb03012d2f6cb662400a4d
Là Đức An Đó <me@clgt.io>
2019-11-18 10:49:43 +0700

    update logo

--- ui/sidebar/logo.tsx
+++ ui/sidebar/logo.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import styled from "styled-components";
-import logo from "../assets/logo.png";
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo.png'
 
 const Container = styled.div`
   font-size: 25px;
   text-align: center;
   margin-bottom: 15px;
-`;
+`
 
 export default function Logo() {
-  return (
-    <Container>
-      <img width={200} src={logo} />
-    </Container>
-  );
+  return <Container>IS_FIBER_READY_YET</Container>
 }
